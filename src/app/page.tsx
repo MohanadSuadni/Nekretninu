0e263d4a975e714a0ac943a7cad8708480ac3df3
Mohanad Suadni <mohanadsuadni@gmail.com>
2026-01-27 11:16:08 +0100

    for blody

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 // 📁 src/app/page.tsx
 export const dynamic = 'force-dynamic';
 
-import React from 'react';
 import { Metadata } from "next";
+import Script from "next/script";
+
 import Hero from './components/home/hero';
 import Calculator from './components/home/calculator';
 import History from './components/home/history';
@@ -19,16 +20,38 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main>
-      <Hero />
-      <DiscoverProperties />
-      <Listing />
-      <Calculator />
-      <Features />
-      <History />
-      <Testimonials />
-      <CompanyInfo />
-      <BlogSmall />
-    </main>
+    <>
+      {/* Tawk.to chat */}
+      <Script
+        id="tawk-to"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+              var s1=document.createElement("script"),
+                  s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/69787a19bef0aa197b29c135/1jfv9ptg7';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
+          `,
+        }}
+      />
+
+      <main>
+        <Hero />
+        <DiscoverProperties />
+        <Listing />
+        <Calculator />
+        <Features />
+        <History />
+        <Testimonials />
+        <CompanyInfo />
+        <BlogSmall />
+      </main>
+    </>
   );
 }
